6bdf87fbda0040581801d396200a394b8b52da19
Tsonewa <tsonewa@gmail.com>
2021-12-17 16:48:15 +0200

    initial commit

---
+++ src/main/java/com/example/payrollrest/PayrollRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.payrollrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/payrollrest/PayrollRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payrollrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
